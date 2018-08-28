6903f87c128d9503a43068ac1d30599e76c7fcb5
Lloyd Boone <lloyd@lambapps.com>
2018-08-28 17:18:33 -0400

    Event Navbar Components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+
+// Import
+import NavBar from "../../features/nav/NavBar/NavBar";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents</h1>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
