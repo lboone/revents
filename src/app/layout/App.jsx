a3e4d75c2b826d8f66f0625cd6015f6808d6ff51
Lloyd Boone <lloyd@lambapps.com>
2018-08-30 18:46:26 -0400

    Add Redux Store

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -11,6 +11,7 @@ import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
 import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
 import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import TestComponent from "../../features/testarea/TestComponent";
 
 class App extends Component {
   render() {
@@ -28,6 +29,7 @@ class App extends Component {
               <Container className="main">
                 <Switch>
                   <Route path="/events" component={EventDashboard} />
+                  <Route path="/test" component={TestComponent} />
                   <Route path="/event/:id" component={EventDetailedPage} />
                   <Route path="/people" component={PeopleDashboard} />
                   <Route path="/profile/:id" component={UserDetailedPage} />
---
+++ src/features/testarea/TestComponent.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class TestComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Test Area</h1>
+        <h3>The answer is: {this.props.data}</h3>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  data: state.test.data
+});
+export default connect(mapStateToProps)(TestComponent);
