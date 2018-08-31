15a2e1405d44abddc527aed97e5970ae198467e8
Lloyd Boone <lloyd@lambapps.com>
2018-08-31 13:37:36 -0400

    Add Redux Test Component and Files

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { incrementCounter, decrementCounter } from "./testActions";
 
 class TestComponent extends Component {
   render() {
+    const { incrementCounter, decrementCounter, data } = this.props;
     return (
       <div>
         <h1>Test Area</h1>
-        <h3>The answer is: {this.props.data}</h3>
+        <h3>The answer is: {data}</h3>
+        <Button onClick={incrementCounter} color="green" content="Increment" />
+        <Button onClick={decrementCounter} color="red" content="Decrement" />
       </div>
     );
   }
@@ -14,4 +19,11 @@ class TestComponent extends Component {
 const mapStateToProps = state => ({
   data: state.test.data
 });
-export default connect(mapStateToProps)(TestComponent);
+const mapDispatchToActions = {
+  incrementCounter,
+  decrementCounter
+};
+export default connect(
+  mapStateToProps,
+  mapDispatchToActions
+)(TestComponent);
