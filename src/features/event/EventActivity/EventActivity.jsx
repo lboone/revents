fc3b6da5a3c40109540a57b0b5a47aa6b671cec1
Lloyd Boone <lloyd@lambapps.com>
2018-09-11 17:07:51 -0400

    Add Recent Activity to Event Dashboard

---
+++ src/features/event/EventActivity/EventActivity.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header, Segment } from "semantic-ui-react";
+
+const EventActivity = () => {
+  return (
+    <div>
+      <Header attached="top" content="Recent Activity" />
+      <Segment attached>Recent Activity</Segment>
+    </div>
+  );
+};
+
+export default EventActivity;
